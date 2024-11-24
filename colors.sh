80b14ed61dbf9d24e590cfb9192e3cde208f7cf8
Orange4Broom <orang4broom@gmail.com>
2024-11-24 14:26:12 +0100

    Adding file for colors and config.example

---
+++ colors.sh
@@ -0,0 +1,5 @@
+# Colors
+RED='\033[31m'   # Red color for error messages
+GREEN='\033[32m' # Green color for success messages
+CYAN='\033[36m'  # Cyan color for user prompts
+NC='\033[0m'     # Reset color
---
+++ config.example.sh
@@ -0,0 +1,6 @@
+#config
+WORK_USERNAME="work_username"
+WORK_EMAIL="work_email"
+
+HOME_USERNAME="home_username"
+HOME_EMAIL="home_email"

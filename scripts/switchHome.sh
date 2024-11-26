ad80b91f7012e309b7e1905b98152a76283038f9
Orange4Broom <orang4broom@gmail.com>
2024-11-26 23:56:42 +0100

    Adding unset the name and email and textation

--- scripts/switchHome.sh
+++ scripts/switchHome.sh
@@ -5,7 +5,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../colors.sh"
 source "$SCRIPT_DIR/../config.sh"
 
+git config --global --unset user.name || true
+git config --global --unset user.email || true
+
 echo -e "${CYAN}Přepínám na home git account${NC}"
 git config --global user.name $HOME_USERNAME
 git config --global user.email $HOME_EMAIL
 echo -e "${GREEN}Přepnuto na home${NC}"
+
+echo -e "${CYAN}Upravený Git Configuration:${NC}"
+git config --list | grep 'user.name\|user.email' | head -n 2
--- scripts/switchWork.sh
+++ scripts/switchWork.sh
@@ -5,7 +5,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../colors.sh"
 source "$SCRIPT_DIR/../config.sh"
 
+git config --global --unset user.name || true
+git config --global --unset user.email || true
+
 echo -e "${CYAN}Přepínám na work git account${NC}"
 git config --global user.name $WORK_USERNAME
 git config --global user.email $WORK_EMAIL
 echo -e "${GREEN}Přepnuto na work${NC}"
+
+echo -e "${CYAN}Upravený Git Configuration:${NC}"
+git config --list | grep 'user.name\|user.email' | head -n 2
